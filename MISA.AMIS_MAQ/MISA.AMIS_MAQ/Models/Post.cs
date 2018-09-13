68ba759f01de2d9f77223c92ff5a7db822ea8c0b
Phạm Duy Anh <37221121+anhpd1004@users.noreply.github.com>
2018-09-13 20:54:10 +0700

    khung giao diện
    Duy anh - 9/13/2018
    Đã tạo khung giao diện, phân chia vùng từng người.
    Đã hoàn thành header, sidebar và các sub menu liên quan.

---
+++ MISA.AMIS_MAQ/MISA.AMIS_MAQ/Models/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MISA.AMIS_MAQ.Models
+{
+    public class Post
+    {
+        /// <summary>
+        /// Post id
+        /// </summary>
+        public int PostID { get; set; }
+        /// <summary>
+        /// customerID
+        /// </summary>
+        public int CustomerID { get; set; }
+        /// <summary>
+        /// like id
+        /// </summary>
+        public int LikeID { get; set; }
+        /// <summary>
+        /// comment id
+        /// </summary>
+        public int CommentID { get; set; }
+        /// <summary>
+        /// nội dung bài post
+        /// </summary>
+        public string PostContent { get; set; }
+    }
+}
---
+++ MISA.AMIS_MAQ/MISA.AMIS_MAQ/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MISA.AMIS_MAQ.Models
+{
+    public class User
+    {
+        public int CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerCode { get; set; }
+        public string CompanyName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Numberphone { get; set; }
+        public string Gender { get; set; }
+    }
+}
